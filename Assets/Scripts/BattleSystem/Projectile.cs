993081f69a95494a097f68130723327bc1410331
Ryan Abusaada <71282474+gZation@users.noreply.github.com>
2024-05-14 23:07:14 -0400

    Projectiles in wowrks

---
+++ Assets/Scripts/BattleSystem/Projectile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : Conductable
+{
+    [Header("Projectile Specs")]
+    [SerializeField] private int _dmg;
+    public void Spawn()
+    {
+
+    }
+    protected override void OnBeat()
+    {
+
+    }
+}
