14161a136b26739b2f15c59c695f8d1aad1207ac
Ryan Abusaada <71282474+gZation@users.noreply.github.com>
2024-08-27 23:14:29 -0400

    Update CameraConfigure.cs

--- Assets/Scripts/Camera/CameraConfigure.cs
+++ Assets/Scripts/Camera/CameraConfigure.cs
@@ -5,21 +5,37 @@ using Cinemachine;
 
 public class CameraConfigure : Singleton<CameraConfigure>
 {
-    private CinemachineVirtualCamera prev;
+    private CinemachineVirtualCamera[] prev;
+    private CinemachineVirtualCamera curr;
+    private int[] prevPriority;
+    private int currOgPriority;
     #region Unity Messages
     private void Awake()
     {
         InitializeSingleton();
+        curr = CinemachineBrain.SoloCamera as CinemachineVirtualCamera;
+        prevPriority = new int[2];
+        prev = new CinemachineVirtualCamera[] { curr, curr };
     }
     #endregion
     public void SwitchToCamera(CinemachineVirtualCamera targetCamera)
     {
-        prev = CinemachineBrain.SoloCamera as CinemachineVirtualCamera;
-        CinemachineBrain.SoloCamera = targetCamera;
+        prevPriority[1] = prev[0].Priority;
+        prev[1] = prev[0];
+        prev[1].Priority = 0;
+
+        prevPriority[0] = curr.Priority;
+        prev[0] = curr;
+        prev[0].Priority = 0;
+
+        curr = targetCamera;
+        currOgPriority = curr.Priority;
+        curr.Priority = 100;   
     }
     public void SwitchBackToPrev()
     {
-        if (prev == null) return;
-        CinemachineBrain.SoloCamera = prev;
+        prev[1].Priority = prevPriority[1];
+        prev[0].Priority = prevPriority[0];
+        prev[]
     }
 }
