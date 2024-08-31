36ec94aeb276b611f9dca3aaf01a69c061be1902
Ryan A. <ryanabusa8134@gmail.com>
2024-08-30 22:29:02 -0400

    Created Main Scene Folder

--- Assets/_Scripts/Camera/Billboard.cs
+++ Assets/_Scripts/Camera/Billboard.cs
@@ -6,7 +6,8 @@ public class Billboard : MonoBehaviour
 {
     private void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        //transform.LookAt(Camera.main.transform.position, Vector3.up);
+        transform.forward = Camera.main.transform.forward;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
     }
 }
