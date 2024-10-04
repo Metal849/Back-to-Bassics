d677be52568660bb13d3ebd989042f11740635e0
Ryan Abusaada <71282474+gZation@users.noreply.github.com>
2024-10-03 23:37:52 -0400

    Fixed Death State

--- Assets/_Scripts/StateSystem/SpritePosition/Center.cs
+++ Assets/_Scripts/StateSystem/SpritePosition/Center.cs
@@ -2,6 +2,10 @@ public partial class PositionStateMachine
 {
     public class Center: PositionState
     {
-        
+        public override void Enter(PositionStateInput i)
+        {
+            base.Enter(i);
+            Input.PawnSprite?.Animator.Play("standby");
+        }
     }
 }
